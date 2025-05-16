1b5eaa988e2329dee6f6bc0180ea5c72d4d52a0a
pyhtinps <ppyhtin@ppr.ru>
2025-05-16 18:48:46 +0300

    iter14 race fix

--- cmd/agent/main_test.go
+++ cmd/agent/main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"testing"
 	"time"
 
@@ -11,10 +12,19 @@ import (
 
 type MockMetricsProvider struct {
 	CollectCalledTimes int
+	*sync.Mutex
+}
+
+func (m *MockMetricsProvider) Get() int {
+	m.Lock()
+	defer m.Unlock()
+	return m.CollectCalledTimes
 }
 
 func (m *MockMetricsProvider) Collect() {
+	m.Lock()
 	m.CollectCalledTimes++
+	m.Unlock()
 }
 
 func (m *MockMetricsProvider) AsMap() *map[string]interface{} {
@@ -23,10 +33,18 @@ func (m *MockMetricsProvider) AsMap() *map[string]interface{} {
 
 type MockMetricsConsumer struct {
 	UpdateCalledTimes int
+	*sync.Mutex
 }
 
+func (m *MockMetricsConsumer) Get() int {
+	m.Lock()
+	defer m.Unlock()
+	return m.UpdateCalledTimes
+}
 func (m *MockMetricsConsumer) Update() {
+	m.Lock()
 	m.UpdateCalledTimes++
+	m.Unlock()
 }
 
 func TestRunCollectionAndUpdate(t *testing.T) {
@@ -36,24 +54,30 @@ func TestRunCollectionAndUpdate(t *testing.T) {
 
 	fmt.Println("Testing RunCollectionAndUpdate() started")
 
-	provider := &MockMetricsProvider{}
-	consumer := &MockMetricsConsumer{}
+	provider := &MockMetricsProvider{
+		0,
+		&sync.Mutex{},
+	}
+	consumer := &MockMetricsConsumer{
+		0,
+		&sync.Mutex{},
+	}
 
 	RunCollectionAndUpdate(provider, pollInterval, consumer, reportInterval)
 
 	time.Sleep(100 * time.Millisecond)
 
-	assert.Equal(t, 1, provider.CollectCalledTimes)
+	assert.Equal(t, 1, provider.Get())
 
 	time.Sleep(pollInterval)
 
-	assert.Equal(t, 2, provider.CollectCalledTimes)
+	assert.Equal(t, 2, provider.Get())
 
 	time.Sleep(reportInterval - pollInterval)
 
-	assert.Equal(t, 1, consumer.UpdateCalledTimes)
+	assert.Equal(t, 1, consumer.Get())
 
 	time.Sleep(reportInterval)
 
-	assert.Equal(t, 2, consumer.UpdateCalledTimes)
+	assert.Equal(t, 2, consumer.Get())
 }
